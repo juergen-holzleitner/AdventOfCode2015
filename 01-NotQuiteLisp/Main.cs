525e561c4bb2f492cc93098e6f0347912ff8eb29
Juergen Holzleitner <juergen@juergen-holzleitner.com>
2022-11-19 16:14:22 +0100

    Part1 of NotQuiteLisp

---
+++ 01-NotQuiteLisp/Main.cs
@@ -0,0 +1,8 @@
+﻿using _01_NotQuiteLisp;
+
+var input = File.ReadAllText("input.txt");
+
+var floorEvaluator = new FloorEvaluator();
+floorEvaluator.ProcessInput(input);
+
+Console.WriteLine($"Part1: final floor: {floorEvaluator.GetFloor()}");
