944dc04de0bea55921df3bac1b6c769dbfd4b5b9
Juergen Holzleitner <juergen@juergen-holzleitner.com>
2023-01-08 13:08:12 +0100

    get all permutations

---
+++ 13-KnightsoftheDinnerTable/Dinner.cs
@@ -0,0 +1,37 @@
+﻿namespace _13_KnightsoftheDinnerTable
+{
+  internal class Dinner
+  {
+    internal static IEnumerable<List<int>> GetAllPermutationsOf(int n)
+    {
+      var initial = Enumerable.Range(0, n).ToList();
+
+      return GetPermutations(initial, 0);
+    }
+
+    private static IEnumerable<List<int>> GetPermutations(List<int> currentList, int currentPos)
+    {
+      if (currentPos + 1 >= currentList.Count)
+        yield return currentList;
+      else
+      {
+        for (int n = currentPos; n < currentList.Count; ++n)
+        {
+          Swap(currentList, currentPos, n);
+          foreach (var p in GetPermutations(currentList, currentPos + 1))
+            yield return p;
+          Swap(currentList, n, currentPos);
+        }
+      }
+    }
+
+    private static void Swap(List<int> currentList, int a, int b)
+    {
+      if (a == b)
+        return;
+
+      (currentList[b], currentList[a]) = (currentList[a], currentList[b]);
+    }
+
+  }
+}
--- 13-KnightsoftheDinnerTable/DinnerTest.cs
+++ 13-KnightsoftheDinnerTable/DinnerTest.cs
@@ -1,11 +1,18 @@
+using FluentAssertions;
+
 namespace _13_KnightsoftheDinnerTable
 {
   public class DinnerTest
   {
     [Fact]
-    public void Test1()
+    public void Can_get_permutations()
     {
+      int number = 5;
+      int expectedElements = 5 * 4 * 3 * 2 * 1;
 
+      var numElements = Dinner.GetAllPermutationsOf(number).Count();
+
+      numElements.Should().Be(expectedElements);
     }
   }
 }
